391feb555b675c8ad18f870a194cbc536cc582a1
Emin Deniz <emindeniz99@gmail.com>
2020-11-23 22:08:37 +0300

    hide "Notion Log In and Copy Link" Mobile
    fix some part of this issue https://github.com/stephenou/fruitionsite/issues/60

--- worker.js
+++ worker.js
@@ -189,6 +189,7 @@ class HeadRewriter {
       div.notion-topbar > div > div:nth-child(6) { display: none !important; }
       div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
       div.notion-topbar-mobile > div:nth-child(4) { display: none !important; }
+      div.notion-topbar-mobile > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
       div.notion-topbar-mobile > div:nth-child(1n).toggle-mode { display: block !important; }
       </style>`,
