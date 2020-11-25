7f273edcd4f7ea4707663b3209b9d1eb05aac0bb
Stephen Ou <me@stephenou.com>
2020-11-25 15:55:34 -0800

    block unlisted pages from other domains from loading

--- worker.js
+++ worker.js
@@ -122,6 +122,11 @@ async function fetchAndApply(request) {
   } else if (slugs.indexOf(url.pathname.slice(1)) > -1) {
     const pageId = SLUG_TO_PAGE[url.pathname.slice(1)];
     return Response.redirect("https://" + MY_DOMAIN + "/" + pageId, 301);
+  } else if (
+    pages.indexOf(url.pathname.slice(1)) === -1 &&
+    url.pathname.slice(1).match(/[0-9a-f]{32}/)
+  ) {
+    return Response.redirect('https://' + MY_DOMAIN, 301);
   } else {
     response = await fetch(url.toString(), {
       body: request.body,
