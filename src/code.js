ed4770e0eb683f9714caf94a1daec06f20cdc80d
Alex Laughnan <alaughnan@box.com>
2021-08-13 22:47:10 -0700

    Override domainBaseUrl per Notion recommendation
    https://www.reddit.com/r/Notion/comments/p3wgyn/is_it_just_happening_to_me_fruitionsitecom/h8vhdmn

--- src/code.js
+++ src/code.js
@@ -22,10 +22,10 @@ export default function code(data) {
     if (url.slice(-1) === '/') url = url.slice(0, url.length - 1);
 
     return `  /* CONFIGURATION STARTS HERE */
-  
+
   /* Step 1: enter your domain name like fruitionsite.com */
   const MY_DOMAIN = '${url}';
-  
+
   /*
    * Step 2: enter your URL slug to page ID mapping
    * The key on the left is the slug (without the slash)
@@ -40,19 +40,19 @@ ${slugs
         return `    '${pageUrl}': '${id}',\n`;
     })
     .join('')}  };
-  
+
   /* Step 3: enter your page title and description for SEO purposes */
   const PAGE_TITLE = '${pageTitle || ''}';
   const PAGE_DESCRIPTION = '${pageDescription || ''}';
-  
+
   /* Step 4: enter a Google Font name, you can choose from https://fonts.google.com */
   const GOOGLE_FONT = '${googleFont || ''}';
-  
+
   /* Step 5: enter any custom scripts you'd like */
   const CUSTOM_SCRIPT = \`${customScript || ''}\`;
-  
+
   /* CONFIGURATION ENDS HERE */
-  
+
   const PAGE_TO_SLUG = {};
   const slugs = [];
   const pages = [];
@@ -62,7 +62,7 @@ ${slugs
     pages.push(page);
     PAGE_TO_SLUG[page] = slug;
   });
-  
+
   addEventListener('fetch', event => {
     event.respondWith(fetchAndApply(event.request));
   });
@@ -77,13 +77,13 @@ ${slugs
     sitemap += '</urlset>';
     return sitemap;
   }
-  
+
   const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
   };
-  
+
   function handleOptions(request) {
     if (request.headers.get('Origin') !== null &&
       request.headers.get('Access-Control-Request-Method') !== null &&
@@ -101,7 +101,7 @@ ${slugs
       });
     }
   }
-  
+
   async function fetchAndApply(request) {
     if (request.method === 'OPTIONS') {
       return handleOptions(request);
@@ -152,10 +152,10 @@ ${slugs
       response.headers.delete('Content-Security-Policy');
       response.headers.delete('X-Content-Security-Policy');
     }
-  
+
     return appendJavascript(response, SLUG_TO_PAGE);
   }
-  
+
   class MetaRewriter {
     element(element) {
       if (PAGE_TITLE !== '') {
@@ -183,7 +183,7 @@ ${slugs
       }
     }
   }
-  
+
   class HeadRewriter {
     element(element) {
       if (GOOGLE_FONT !== '') {
@@ -206,7 +206,7 @@ ${slugs
       })
     }
   }
-  
+
   class BodyRewriter {
     constructor(SLUG_TO_PAGE) {
       this.SLUG_TO_PAGE = SLUG_TO_PAGE;
@@ -214,6 +214,7 @@ ${slugs
     element(element) {
       element.append(\`<div style="display:none">Powered by <a href="http://fruitionsite.com">Fruition</a></div>
       <script>
+      window.CONFIG.domainBaseUrl = "https://${MY_DOMAIN}";
       const SLUG_TO_PAGE = \${JSON.stringify(this.SLUG_TO_PAGE)};
       const PAGE_TO_SLUG = {};
       const slugs = [];
@@ -312,7 +313,7 @@ ${slugs
       });
     }
   }
-  
+
   async function appendJavascript(res, SLUG_TO_PAGE) {
     return new HTMLRewriter()
       .on('title', new MetaRewriter())
