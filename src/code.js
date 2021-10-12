386f17ca9a13459a45825e5b74e9d64120fe16b3
Ben Foxall <benfoxall@gmail.com>
2021-10-12 13:58:03 +0100

    Update `code.js` to match `worker.js`

--- src/code.js
+++ src/code.js
@@ -211,7 +211,7 @@ ${slugs
     element(element) {
       element.append(\`<div style="display:none">Powered by <a href="http://fruitionsite.com">Fruition</a></div>
       <script>
-      window.CONFIG.domainBaseUrl = 'https://\${MY_DOMAIN}';
+      window.CONFIG.domainBaseUrl = location.origin;
       const SLUG_TO_PAGE = \${JSON.stringify(this.SLUG_TO_PAGE)};
       const PAGE_TO_SLUG = {};
       const slugs = [];
