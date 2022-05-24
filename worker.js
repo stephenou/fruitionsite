b5792fe89f6cfc1812734a52e650ec6dc796de3b
Landon <34496757+landoncrabtree@users.noreply.github.com>
2022-05-23 23:50:25 -0400

    Update worker.js
    Adds 'theme' cookie to keep theme persistence. Overrides system preferences on dark or light based on what the cookie is set to. Useful for when a user has dark theme but wants light browsing, or vice versa.

--- worker.js
+++ worker.js
@@ -236,15 +236,40 @@ class BodyRewriter {
           history.replaceState(history.state, '', '/' + slug);
         }
       }
+      // Write Cookie
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      // Get Cookie
+      function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
+      }
       function onDark() {
         el.innerHTML = '<div title="Change to Light Mode" style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgb(46, 170, 220); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(12px) translateY(0px);"></div></div></div></div>';
         document.body.classList.add('dark');
         __console.environment.ThemeStore.setState({ mode: 'dark' });
+        setCookie('theme', 'dark', 30)
       };
       function onLight() {
         el.innerHTML = '<div title="Change to Dark Mode" style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgba(135, 131, 120, 0.3); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(0px) translateY(0px);"></div></div></div></div>';
         document.body.classList.remove('dark');
         __console.environment.ThemeStore.setState({ mode: 'light' });
+        setCookie('theme', 'light', 30)
       }
       function toggle() {
         if (document.body.classList.contains('dark')) {
@@ -260,16 +285,26 @@ class BodyRewriter {
         nav.appendChild(el);
 
         // enable smart dark mode based on user-preference
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            onDark();
-        } else {
-            onLight();
+        // set cookies should override this.
+        if (getCookie('theme') == "") {
+          if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+              onDark();
+            } else {
+              onLight();
+            }
         }
 
         // try to detect if user-preference change
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
             toggle();
         });
+        if (getCookie('theme') == 'light') {
+          onLight();
+        } else if (getCookie('theme') == 'dark') {
+          onDark();
+        } else {
+          onDark();
+        }
       }
       const observer = new MutationObserver(function() {
         if (redirected) return;
