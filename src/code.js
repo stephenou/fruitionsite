107debcdc9d4bc42b8863c452935a3be6db9653e
Landon <34496757+landoncrabtree@users.noreply.github.com>
2022-05-23 23:27:22 -0400

    Update code.js
    Add cookies to keep user's theme (light or dark) persistent across sessions.

--- src/code.js
+++ src/code.js
@@ -246,19 +246,49 @@ ${slugs
         document.body.classList.remove('dark');
         __console.environment.ThemeStore.setState({ mode: 'light' });
       }
-      function toggle() {
+           function toggle() {
         if (document.body.classList.contains('dark')) {
           onLight();
+          setCookie('theme', 'light', 30);
         } else {
           onDark();
+          setCookie('theme', 'dark', 30);
         }
+      }
+      function setCookie(cname, cvalue, exdays) {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+          let c = ca[i];
+          while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+          }
+          if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+          }
+        }
+        return "";
       }
       function addDarkModeButton(device) {
         const nav = device === 'web' ? document.querySelector('.notion-topbar').firstChild : document.querySelector('.notion-topbar-mobile');
         el.className = 'toggle-mode';
         el.addEventListener('click', toggle);
         nav.appendChild(el);
-        onLight();
+        if (getCookie('theme') == 'light') {
+          onLight();
+        } else if (getCookie('theme') == 'dark') {
+          onDark();
+        } else {
+          onDark();
+          setCookie('theme', 'dark', 30);
+        }
       }
       const observer = new MutationObserver(function() {
         if (redirected) return;
