71e86845eb9b3374e8ff67d84487e96d1d15555e
shigahi <masanari.higashi@classmethod.de>
2023-04-27 00:11:45 +0200

    package update MUI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App from "./App.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
