056c30d8efb0da6d9e2261eda57674b794da7729
shigahi <masanari.higashi@classmethod.de>
2023-05-05 14:51:16 +0200

    hiding Notion button

--- worker.js
+++ worker.js
@@ -192,6 +192,7 @@ class HeadRewriter {
       div.notion-topbar > div > div:nth-child(4) { display: none !important; }
       div.notion-topbar > div > div:nth-child(5) { display: none !important; }
       div.notion-topbar > div > div:nth-child(6) { display: none !important; }
+      div.notion-topbar > div > div:nth-child(7) { display: none !important; }
       div.notion-topbar-mobile > div:nth-child(3) { display: none !important; }
       div.notion-topbar-mobile > div:nth-child(4) { display: none !important; }
       div.notion-topbar > div > div:nth-child(1n).toggle-mode { display: block !important; }
