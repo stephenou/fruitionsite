ead6be357cf3818a0a583d26698cc1a1b8c76d81
leoua7 <Leoua7@163.com>
2023-09-25 04:40:30 +0000

    fix light/dark mode toggle by tnkemdilim

--- worker.js
+++ worker.js
@@ -246,6 +246,7 @@ class BodyRewriter {
         __console.environment.ThemeStore.setState({ mode: 'dark' });
       };
       function onLight() {
+        if (!__console.isEnabled) __console.enable();
         el.innerHTML = '<div title="Change to Dark Mode" style="margin-left: auto; margin-right: 14px; min-width: 0px;"><div role="button" tabindex="0" style="user-select: none; transition: background 120ms ease-in 0s; cursor: pointer; border-radius: 44px;"><div style="display: flex; flex-shrink: 0; height: 14px; width: 26px; border-radius: 44px; padding: 2px; box-sizing: content-box; background: rgba(135, 131, 120, 0.3); transition: background 200ms ease 0s, box-shadow 200ms ease 0s;"><div style="width: 14px; height: 14px; border-radius: 44px; background: white; transition: transform 200ms ease-out 0s, background 200ms ease-out 0s; transform: translateX(0px) translateY(0px);"></div></div></div></div>';
         document.body.classList.remove('dark');
         __console.environment.ThemeStore.setState({ mode: 'light' });
